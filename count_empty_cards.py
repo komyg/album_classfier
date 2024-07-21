117011f075ca37becca7fb003bc554bd5ce07ec2
Felipe Armoni <felipe.armoni@gmail.com>
2024-07-21 14:02:37 -0300

    Added count cards example

--- count_empty_cards.py
+++ count_empty_cards.py
@@ -10,9 +10,6 @@ load_dotenv()
 detect_client = InferenceHTTPClient(
     api_url="https://detect.roboflow.com", api_key=os.environ["ROBOFLOW_API_KEY"]
 )
-infer_client = InferenceHTTPClient(
-    api_url="https://infer.roboflow.com", api_key=os.environ["ROBOFLOW_API_KEY"]
-)
 
 
 def show_annotations(detections, inference_image):
@@ -32,27 +29,28 @@ def show_annotations(detections, inference_image):
     sv.plot_image(annotated_image)
 
 
+def get_all_images(directory: str) -> list[str]:
+    files = []
+    for file in os.listdir(directory):
+        file_with_path = os.path.join(directory, file)
+        if os.path.isfile(file_with_path):
+            files.append(file_with_path)
+    return files
+
+
 def locate_empty_cards(image: ImageFile) -> sv.Detections:
     empty_cards = detect_client.infer(inference_input=image, model_id="album-wrkp7/1")
     detections = sv.Detections.from_inference(empty_cards)
     return detections
 
 
-def get_empty_cards_numbers(detections: sv.Detections, image: ImageFile) -> list:
-    cards_numbers = []
-    print(detections)
-    for coordinates in detections.xyxy:
-        x0, y0, x1, y1 = coordinates
-        cropped_image = image.crop((x0, y0, x1, y1)).convert("L")
-        sv.plot_image(cropped_image)
-        card_number = infer_client.ocr_image(cropped_image)
-        cards_numbers.append(card_number)
-        print(card_number)
-
-    return cards_numbers
+total_empty_cards = 0
+images = get_all_images("imgs/test")
+for img in images:
+    image = Image.open(img)
+    empty_cards = locate_empty_cards(image)
+    total_empty_cards += len(empty_cards.xyxy)
+    print(f"Empty cards in {img}: {len(empty_cards.xyxy)}")
 
 
-image_file = "imgs/test/enaldinho_16.jpeg"
-image = Image.open(image_file)
-empty_cards = locate_empty_cards(image)
-get_empty_cards_numbers(empty_cards, image)
+print(f"Total empty cards: {total_empty_cards}")  # Result: 89, Real Result: 103
